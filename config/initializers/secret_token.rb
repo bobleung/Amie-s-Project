c06c5a62b6c35c58cbdbc72c55a7b8f5570805d2
Bob Leung <bob@woobius.com>
2011-01-03 10:48:56 +0000

    Second time lucky

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmieSystem::Application.config.secret_token = 'c0542c51ae3f9310b0e5f9f54eb802c2eb3284bc3980fbe35e92c4f8a960658e1343853d3e2443edd8c098923eec104c52317f19f39c1e99b82794b94330e559'
